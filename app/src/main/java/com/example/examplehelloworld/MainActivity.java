517cdfdca87e03eb95dae9827dfa578bed0c4101
Vishwam Shukla <shuklavishwam21@gmail.com>
2020-07-21 11:41:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/examplehelloworld/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.examplehelloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    public void fade(View view){
+        Log.i("Info", "Image Clicked");
+        ImageView imageView = findViewById(R.id.imageView1);
+        imageView.animate().alpha(0).setDuration(2000);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button2);
+    }
+}
